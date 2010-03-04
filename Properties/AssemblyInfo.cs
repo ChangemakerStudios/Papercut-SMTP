81a4458d18ea129189370c1662237dd46ee2ac81
krobertson <none@none.com>
2010-03-04 02:01:52 +0000

    Upped version for 2010.3.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2009.12.18.0")]
-[assembly: AssemblyFileVersion("2009.12.18.0")]
+[assembly: AssemblyVersion("2010.3.3.0")]
+[assembly: AssemblyFileVersion("2010.3.3.0")]
