11605c69645758efa6a4298044ea9143a60cce60
Jaben <jaben-accounts@tinygecko.com>
2013-03-20 21:45:19 -0400

    Update to 2013 version

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ken Robertson")]
 [assembly: AssemblyProduct("Papercut")]
-[assembly: AssemblyCopyright("Copyright © 2008 - 2012")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2012.1.0.0")]
-[assembly: AssemblyFileVersion("2012.1.0.0")]
+[assembly: AssemblyVersion("2013.3.0.0")]
+[assembly: AssemblyFileVersion("2013.3.0.0")]
