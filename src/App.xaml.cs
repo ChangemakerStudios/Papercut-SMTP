d543adbb25ec5fd2e9bc9046af58a47f3303fe5f
Jaben <jaben@yetanotherforum.net>
2014-05-12 00:46:16 -0400

    Missing Copyright -- always forget them.

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,18 +1,33 @@
-﻿namespace Papercut
+﻿/*  
+ * Papercut
+ *
+ *  Copyright © 2008 - 2012 Ken Robertson
+ *  Copyright © 2013 - 2014 Jaben Cargman
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+
+namespace Papercut
 {
     using System;
-    using System.Collections.Generic;
     using System.Diagnostics;
-    using System.IO;
-    using System.Linq;
-    using System.Reflection;
     using System.Windows;
 
     using Autofac;
 
     using Papercut.Core;
     using Papercut.Core.Events;
-    using Papercut.Core.Helper;
     using Papercut.Helpers;
 
     public partial class App : Application
@@ -39,10 +54,10 @@
         {
             var publishEvent = Container.Resolve<IPublishEvent>();
 
-            var appCanStartEvent = new AppPreStartEvent();
-            publishEvent.Publish(appCanStartEvent);
+            var appPreStartEvent = new AppPreStartEvent();
+            publishEvent.Publish(appPreStartEvent);
 
-            if (appCanStartEvent.CancelStart)
+            if (appPreStartEvent.CancelStart)
             {
                 // force shut down...
                 publishEvent.Publish(new AppForceShutdownEvent());
