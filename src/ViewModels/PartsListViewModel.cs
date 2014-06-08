6987fcf3ecba821321f842ae22cf307fcd9ee822
Jaben <jaben@yetanotherforum.net>
2014-06-08 01:17:16 -0400

    Issue #6025 done! "Sections" now lists all mime sections... allow viewing of text sections inside the app and saving of all other sections as a file.

--- src/ViewModels/PartsListViewModel.cs
+++ src/ViewModels/PartsListViewModel.cs
@@ -20,7 +20,9 @@
 
 namespace Papercut.ViewModels
 {
+    using System;
     using System.Collections.ObjectModel;
+    using System.Diagnostics;
     using System.Linq;
 
     using Caliburn.Micro;
@@ -29,14 +31,27 @@ namespace Papercut.ViewModels
 
     using Papercut.Core.Helper;
 
+    using Serilog;
+
     public class PartsListViewModel : Screen
     {
-        public PartsListViewModel()
+        readonly IWindowManager _windowManager;
+
+        readonly ILogger _logger;
+
+        public PartsListViewModel(Func<MimePartViewModel> mimePartViewFactory, IWindowManager windowManager, ILogger logger)
         {
+            _windowManager = windowManager;
+            _logger = logger;
             Parts = new ObservableCollection<MimePart>();
+            _mimePartViewModel = mimePartViewFactory();
         }
 
         MimeMessage _mimeMessage;
+
+        MimePart _selectedPart;
+
+        readonly MimePartViewModel _mimePartViewModel;
 
         public ObservableCollection<MimePart> Parts { get; private set; }
 
@@ -54,6 +69,57 @@ namespace Papercut.ViewModels
                 if (_mimeMessage != null)
                 {
                     RefreshParts();
+                }
+            }
+        }
+
+        public MimePart SelectedPart
+        {
+            get
+            {
+                return _selectedPart;
+            }
+            set
+            {
+                _selectedPart = value;
+                NotifyOfPropertyChange(() => SelectedPart);
+            }
+        }
+
+        public void ViewSection()
+        {
+            var part = this.SelectedPart;
+
+            if (part is TextPart)
+            {
+                var textPart = part as TextPart;
+
+                // show in the viewer...
+                _mimePartViewModel.PartText = textPart.Text;
+                _windowManager.ShowDialog(_mimePartViewModel);
+            }
+            else
+            {
+                var dlg = new Microsoft.Win32.SaveFileDialog();
+                if (!string.IsNullOrWhiteSpace(part.FileName))
+                {
+                    dlg.FileName = part.FileName;
+                }
+
+                var result = dlg.ShowDialog();
+                if (result.HasValue && result.Value)
+                {
+                    _logger.Debug(
+                        "Saving File {File} as Output for MimePart {PartFileName}",
+                        dlg.FileName,
+                        part.FileName);
+
+                    // save it..
+                    using (var outputFile = dlg.OpenFile())
+                    {
+                        part.ContentObject.DecodeTo(outputFile);
+                        outputFile.Close();
+                    }
                 }
             }
         }
---
+++ src/Views/MimePartView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Papercut.Views
+{
+    using MahApps.Metro.Controls;
+
+    /// <summary>
+    ///     Interaction logic for MimePartView.xaml
+    /// </summary>
+    public partial class MimePartView : MetroWindow
+    {
+        public MimePartView()
+        {
+            InitializeComponent();
+        }
+    }
+}
