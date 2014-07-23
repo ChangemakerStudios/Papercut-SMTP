b340f70d2f14c3fb6bd6d3deb6a5648915aa032d
Jaben <jaben@yetanotherforum.net>
2014-07-22 23:26:53 -0400

    v4.0

--- Papercut.Core/Properties/AssemblyInfo.cs
+++ Papercut.Core/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.1.0")]
-[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: AssemblyVersion("4.0.0")]
+[assembly: AssemblyFileVersion("4.0.0")]
