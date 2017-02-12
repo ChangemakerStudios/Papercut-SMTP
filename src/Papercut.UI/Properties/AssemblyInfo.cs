bab86a65943656433fdcf1e8c8a6d54dade15317
Jaben Cargman <jaben-accounts@tinygecko.com>
2017-02-11 22:51:00 -0500

    Don't understand how to do assembly patching yet -- for now just setting new build on assembly info

--- src/Papercut.UI/Properties/AssemblyInfo.cs
+++ src/Papercut.UI/Properties/AssemblyInfo.cs
@@ -67,5 +67,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.6.0.0")]
-[assembly: AssemblyFileVersion("4.6.0.0")]
+[assembly: AssemblyVersion("4.8.0.0")]
+[assembly: AssemblyFileVersion("4.8.0.0")]
