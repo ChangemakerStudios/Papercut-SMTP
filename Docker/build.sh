3ee9cf643e6044ccbd8e4f5e2954572ed5b10938
Jijie Chen <jijie.chen@someplace.com>
2017-09-28 12:26:31 +0800

    Added docker build script for Non-Windows environment

---
+++ Docker/build.sh
@@ -0,0 +1,10 @@
+
+if [ -d ./release/Papercut.Service ]; then
+   rm -rf ./release/Papercut.Service
+fi
+
+dotnet publish src/Papercut.Service -c Release -o ../../release/Papercut.Service
+cp ./Docker/Linux.Dockerfile ./release/Papercut.Service/Dockerfile
+
+time=`date -u +%Y%m%d%H%M%S`
+docker build ./release/Papercut.Service --tag "papercut:$time"
