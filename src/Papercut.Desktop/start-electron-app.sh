e61dd2ffbf6f0a426801f3bfc7e260f57bb1fb0d
Jijie Chen <jijie.chen@someplace.com>
2018-03-30 15:15:36 +0800

    Revert "Reduce size of packaged application for the macOS by using gulp-atom-electron to replace electron-packager"
    This reverts commit c7b417c30a25836990566be04a8c0e60ef0b14b9.

--- src/Papercut.Desktop/start-electron-app.sh
+++ src/Papercut.Desktop/start-electron-app.sh
@@ -7,6 +7,6 @@ cp -f obj/Host/bin/main.js obj/Host/main.js
 cp -f obj/Host/bin/package.json obj/Host/package.json
 cp -f obj/Host/bin/launch.sh obj/Host/launch.sh
 cd obj/Host/
-npm install --production
+npm install
 cd ./node_modules/.bin/
 ./electron ../../main.js
