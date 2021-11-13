b981ff6a366a6be89af61ac938ba415d70394220
Jaben Cargman <jaben@changemakerstudios.us>
2021-11-13 11:46:29 -0500

    Not going to work.

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
-$exePath = "$env:TEMP\wix311.exe"
-(New-Object Net.WebClient).DownloadFile('https://github.com/wixtoolset/wix3/releases/download/wix3111rtm/wix311.exe', $exePath)
-cmd /c start /wait "$exePath" /q
+# $exePath = "$env:TEMP\wix311.exe"
+# (New-Object Net.WebClient).DownloadFile('https://github.com/wixtoolset/wix3/releases/download/wix3111rtm/wix311.exe', $exePath)
+# cmd /c start /wait "$exePath" /q
 
 cd .\build
 .\build.ps1
