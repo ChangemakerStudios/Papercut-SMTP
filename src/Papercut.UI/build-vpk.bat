9c8b3726aa7a3a8f78d95222dcb6c799743822d3
Jaben <jaben@changemakerstudios.us>
2024-05-05 18:41:28 -0400

    Not ultimately going to use a batch file -- but good for getting it working.

--- src/Papercut.UI/build-vpk.bat
+++ src/Papercut.UI/build-vpk.bat
@@ -9,10 +9,11 @@ if "%~1"=="" (
 
 set "version=%~1"
 
-rm .\publish\*.* -r
+echo Y | del .\publish\*.* -r
+echo Y | del .\Releases\*.* -r 
 
 dotnet publish -c Release -r win-x64 -o .\publish\x64
 dotnet publish -c Release -r win-x86 -o .\publish\x86
 
-vpk pack -u PapercutSMTPx64 --packTitle "Papercut SMTP" -r win7-x64 --icon App.ico -v %version% -p .\publish\x64 -o .\releases -e Papercut.exe --framework net8.0-x64-desktop,webview2
-vpk pack -u PapercutSMTPx86 --packTitle "Papercut SMTP" -r win7-x86 --icon App.ico -v %version% -p .\publish\x86 -o .\releases -e Papercut.exe --framework net8.0-x64-desktop,webview2
+vpk pack -u PapercutSMTPx64 --packTitle "Papercut SMTP" --runtime win7-x64 --icon App.ico -v %version% -p .\publish\x64 -o .\releases\x64 -e Papercut.exe --framework net8.0-x64-desktop,webview2
+vpk pack -u PapercutSMTPx86 --packTitle "Papercut SMTP" --runtime win7-x86 --icon App.ico -v %version% -p .\publish\x86 -o .\releases\x86 -e Papercut.exe --framework net8.0-x64-desktop,webview2
