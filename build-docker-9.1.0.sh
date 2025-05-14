9355e183fc2cae61cef693f590c65e1c2c5e92ee
Catalin Mihai Safta <catalin-mihai.safta@ing.com>
2025-05-14 17:00:45 +0300

    updated jquery

---
+++ build-docker-9.1.0.sh
@@ -0,0 +1,5 @@
+
+#bash build-docker.sh 9.1.0
+#docker save docker.io/changemakerstudiosus/papercut-smtp:9.1.0 -o papercut9.1.0.tar
+echo "run with:"
+echo "  docker run -p 37025:37425 -p 37408:37408 changemakerstudiosus/papercut-smtp:9.1.0"
