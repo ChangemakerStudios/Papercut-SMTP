72fd2067cee1c1c3607ca85f5602d9e3fe9db2de
Jaben Cargman <jaben@changemakerstudios.us>
2025-10-26 20:19:47 -0400

    Fix window size saving when minimized
    ISSUE: When the main window was minimized and the application exited,
    it would save the window dimensions (width/height) as zero or minimum
    values. On next launch, the window would open with these incorrect
    minimum dimensions.
    
    ROOT CAUSE: The SettingBinding two-way binding would update settings
    whenever the window dimensions changed, including when the window
    was minimized (which reduces ActualWidth/ActualHeight to minimums).
    
    SOLUTION: Added a WindowDimensionConverter to the SettingBinding that
    intercepts the Window -> Setting updates. When the window is minimized,
    the converter returns Binding.DoNothing, preventing the minimized
    dimensions from being saved. Normal (non-minimized) dimension changes
    are saved as before.
    
    The converter caches a reference to Application.Current.MainWindow
    and checks WindowState before allowing the value to be saved.
    
    Fixes #327

--- src/Papercut.UI/AppLayer/Behaviors/SettingBindingExtension.cs
+++ src/Papercut.UI/AppLayer/Behaviors/SettingBindingExtension.cs
@@ -1,14 +1,14 @@
 ﻿// Papercut
-// 
+//
 // Copyright © 2008 - 2012 Ken Robertson
 // Copyright © 2013 - 2025 Jaben Cargman
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
 // limitations under the License.
 
 
+using System.Windows;
 using System.Windows.Data;
 
 namespace Papercut.AppLayer.Behaviors;
@@ -23,6 +24,8 @@ namespace Papercut.AppLayer.Behaviors;
 /// <summary>
 /// Very useful code from here:
 /// http://tomlev2.wordpress.com/2008/11/18/wpf-binding-to-application-settings-using-a-markup-extension/
+///
+/// Modified to prevent saving window dimensions when minimized (Issue #327)
 /// </summary>
 public class SettingBindingExtension : Binding
 {
@@ -41,5 +44,42 @@ public class SettingBindingExtension : Binding
     {
         this.Source = Properties.Settings.Default;
         this.Mode = BindingMode.TwoWay;
+
+        // Use a value converter to prevent saving dimensions when window is minimized
+        this.Converter = WindowDimensionConverter.Instance;
+    }
+}
+
+/// <summary>
+/// Converter that prevents saving window dimensions when the window is minimized.
+/// This fixes issue #327 where minimized windows would save width/height as zero.
+/// </summary>
+internal class WindowDimensionConverter : IValueConverter
+{
+    public static readonly WindowDimensionConverter Instance = new();
+
+    private Window? _cachedWindow;
+
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        // Setting -> Window: pass through the saved value
+        return value;
+    }
+
+    public object? ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        // Window -> Setting: only save if window is not minimized
+
+        // Try to get window from Application.Current.MainWindow as a fallback
+        _cachedWindow ??= Application.Current?.MainWindow;
+
+        // Don't save dimensions if window is minimized
+        if (_cachedWindow?.WindowState == WindowState.Minimized)
+        {
+            return Binding.DoNothing;
+        }
+
+        // Otherwise, save the value
+        return value;
     }
 }
