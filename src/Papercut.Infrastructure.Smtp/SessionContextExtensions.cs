580750e9ebfae8e7b15e3dbe3cf0a409c87d6edc
Jaben Cargman <jaben@changemakerstudios.us>
2025-11-07 01:40:43 -0500

    Added Null Check.

--- src/Papercut.Infrastructure.Smtp/SessionContextExtensions.cs
+++ src/Papercut.Infrastructure.Smtp/SessionContextExtensions.cs
@@ -1,14 +1,14 @@
 // Papercut
-//
+// 
 // Copyright © 2008 - 2012 Ken Robertson
 // Copyright © 2013 - 2025 Jaben Cargman
-//
+// 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 // http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,6 +36,8 @@ public static class SessionContextExtensions
     /// <returns>The remote IP address, or IPAddress.None if it cannot be determined</returns>
     public static IPAddress GetRemoteIpAddress(this ISessionContext context)
     {
+        ArgumentNullException.ThrowIfNull(context, nameof(context));
+
         if (context.Properties.TryGetValue(RemoteEndPointKey, out var endpointObj)
             && endpointObj is IPEndPoint remoteEndPoint)
         {
